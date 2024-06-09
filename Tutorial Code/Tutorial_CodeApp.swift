6be28c4647422d694fee86cdd11d4d0668890226
Jugz <55399963+JugheadStudio@users.noreply.github.com>
2024-06-09 19:51:37 +0200

    Initial Commit

---
+++ Tutorial Code/Tutorial_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial_CodeApp.swift
+//  Tutorial Code
+//
+//  Created by Ruan Jordaan on 2024/06/09.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
